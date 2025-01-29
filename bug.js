4a7a98e1166bcb50d3734c628809b14bf962000b
BugHunter-X <askzenai@gmail.com>
2025-01-29 01:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+import {unstable_getServerSession} from 'next-auth';
+import {authOptions} from './auth/[...nextauth]';
+
+export default async function handler(req, res) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
+  // ... rest of your API route handler
+}
+```
